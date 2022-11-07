800ccdbbb35c53214bb2aaa0d4acd05fe7124c66
julieta <julieta.martin@clarity.ai>
2022-11-07 16:02:07 +0100

    test: equipment method created for PlayerTest class

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -5,16 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PlayerTest {
 
     // choose this one if you are familiar with mocks
-    @Disabled("Test is not finished yet")
+    //@Disabled("Test is not finished yet")
     @Test
     void damageCalculationsWithMocks() {
         Inventory inventory = mock(Inventory.class);
         Stats stats = mock(Stats.class);
         SimpleEnemy target = mock(SimpleEnemy.class);
+        when(inventory.getEquipment()).thenReturn(equipment());
 
         Damage damage = new Player(inventory, stats).calculateDamage(target);
         assertEquals(10, damage.getAmount());
@@ -29,5 +31,15 @@ public class PlayerTest {
         SimpleEnemy target = new SimpleEnemy(null, null);
         Damage damage = new Player(inventory, stats).calculateDamage(target);
         assertEquals(10, damage.getAmount());
+    }
+
+    Equipment equipment(){
+        Item rightHand = new BasicItem("flashy sword of danger", 10, 1);
+        Item rightHandExcalibur = new BasicItem("excalibur", 0, 0);
+        Item leftHand = new BasicItem("round shield", 0,0);
+        Item feet = new BasicItem("ten league boots", 0, 0);
+        Item head = new BasicItem("helmet of swiftness", 0, 0);
+        Item chest = new BasicItem("breastplate of steel", 0, 0);
+        return new Equipment(rightHand, rightHandExcalibur, leftHand, feet, head, chest);
     }
 }
