e939f3cec37147b1b1cf7be1695a8a3af7d260e4
julieta <julieta.martin@clarity.ai>
2022-11-08 13:29:59 +0100

    refactor: remove SimpleArmor in SimpleArmor class

--- java/src/main/java/codingdojo/SimpleArmor.java
+++ java/src/main/java/codingdojo/SimpleArmor.java
@@ -4,10 +4,6 @@ public class SimpleArmor implements Armor {
 
     private int soak;
 
-    public SimpleArmor(int soak) {
-        this.soak = soak;
-    }
-
     @Override
     public int getDamageSoak() {
         return soak;
