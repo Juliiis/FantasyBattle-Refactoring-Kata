518ccc78fc3f43434aa52d3ad484678bbc7f9dda
julieta <julieta.martin@clarity.ai>
2022-11-08 17:33:06 +0100

    test: damageCalculationForHelmetOfSwiftness test created in PlayerTest class

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -69,6 +69,21 @@ public class PlayerTest {
         assertEquals(0, damage.getAmount());
     }
 
+    @Test
+    void damageCalculationForHelmetOfSwiftness() {
+        Inventory inventory = mock(Inventory.class);
+        Stats stats = mock(Stats.class);
+        SimpleEnemy target = mock(SimpleEnemy.class);
+
+        when(target.getSoakCalculated()).thenReturn(5);
+        when(inventory.getEquipment()).thenReturn(newEquipment());
+        when(inventory.getBaseDamage()).thenReturn(0);
+        when(inventory.getDamageModifier()).thenReturn(1.2f);
+
+        Damage damage = new Player(inventory, stats).calculateDamage(target);
+        assertEquals(0, damage.getAmount());
+    }
+
     Equipment newEquipment(){
         Item rightHand = new BasicItem("flashy sword of danger", 10, 1);
         Item leftHand = new BasicItem("round shield", 0,0);
