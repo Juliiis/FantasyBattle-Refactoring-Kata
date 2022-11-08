e81709228e032ee132a3ad0ee3eca5fc299bdc56
julieta <julieta.martin@clarity.ai>
2022-11-08 15:48:15 +0100

    refactor: removed unused test in PlayerTest class

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -20,20 +20,9 @@ public class PlayerTest {
         when(inventory.getEquipment()).thenReturn(newEquipment());
         when(inventory.getBaseDamage()).thenReturn(10);
         when(inventory.getDamageModifier()).thenReturn(1.0f);
-        
+
         Damage damage = new Player(inventory, stats).calculateDamage(target);
         assertEquals(5, damage.getAmount());
-    }
-
-    // choose this one if you are not familiar with mocks
-    @Disabled("Test is not finished yet")
-    @Test
-    void damageCalculations() {
-        Inventory inventory = new Inventory(null);
-        Stats stats = new Stats(0);
-        SimpleEnemy target = new SimpleEnemy(null, null);
-        Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
     }
 
     Equipment newEquipment(){
