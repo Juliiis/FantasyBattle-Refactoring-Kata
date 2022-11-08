706bb5cf4e48883d5e62fb0998fb863a68418524
julieta <julieta.martin@clarity.ai>
2022-11-08 17:31:46 +0100

    test: damageCalculationForTenLeagueBoots test created in PlayerTest class

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -54,6 +54,21 @@ public class PlayerTest {
         assertEquals(0, damage.getAmount());
     }
 
+    @Test
+    void damageCalculationForTenLeagueBoots() {
+        Inventory inventory = mock(Inventory.class);
+        Stats stats = mock(Stats.class);
+        SimpleEnemy target = mock(SimpleEnemy.class);
+
+        when(target.getSoakCalculated()).thenReturn(5);
+        when(inventory.getEquipment()).thenReturn(newEquipment());
+        when(inventory.getBaseDamage()).thenReturn(0);
+        when(inventory.getDamageModifier()).thenReturn(0.1f);
+
+        Damage damage = new Player(inventory, stats).calculateDamage(target);
+        assertEquals(0, damage.getAmount());
+    }
+
     Equipment newEquipment(){
         Item rightHand = new BasicItem("flashy sword of danger", 10, 1);
         Item leftHand = new BasicItem("round shield", 0,0);
